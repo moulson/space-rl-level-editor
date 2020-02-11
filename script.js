4754981e61eccd189526b3cbd13fc05cb67dceee
Dan Moulson <dan.moulson@wpnc.agency>
2020-02-11 17:28:30 +0000

    publish to json file

--- script.js
+++ script.js
@@ -69,9 +69,19 @@ function exportJson(){
         }
     }
     console.log(jsonRoom);
+    downloadObjectAsJson(jsonRoom, "TEST");
         
 }
 
+function downloadObjectAsJson(exportObj, exportName){
+    var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(exportObj));
+    var downloadAnchorNode = document.createElement('a');
+    downloadAnchorNode.setAttribute("href",     dataStr);
+    downloadAnchorNode.setAttribute("download", exportName + ".json");
+    document.body.appendChild(downloadAnchorNode); // required for firefox
+    downloadAnchorNode.click();
+    downloadAnchorNode.remove();
+  }
 
 //jsonRoom["cells"].filter(x => x.id == "5,1")
 
